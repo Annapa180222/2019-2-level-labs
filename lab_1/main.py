5f2a8783cd9514cbcd8eced6749b41d7040add44
Annapa180222 <nyuta.patrakowa@yandex.ru>
2019-10-06 18:07:35 +0300

    Add

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,25 +1,41 @@
-"""
-Labour work #1
-Count frequencies dictionary by the given arbitrary text
-"""
-
-
 def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    pass
+    delite = ['.', ',', '-', '_', '', '/', '—', '&', '^', '~', '%', '$', '?', '!', '*', '@', '\n']
+    new_txt = {}
+    if type(text)!=str:
+        return new_txt
+    filter_txt = ''
+    for letter in text:
+        if not letter.isdigit() and letter not in delite:
+            filter_txt += letter
+    filter_txt = filter_txt.split()
+    for word in filter_txt:
+        word = word.lower()
+        if word in new_txt:
+            new_txt[word] += 1
+        else:
+            new_txt[word] = 1
+    return new_txt
+
+    print(calculate_frequences(text))
 
 
+def filter_stop_words(frequencies: dict, stop_words: tuple):
+    my_dict = {}
+    if stop_words is None or frequencies is None or len(frequencies) == 0:
+        print('Ошибка')
+        return {}
+    if type(frequencies) is not dict:
+        return {}
+    for key, value in frequencies.items():
+        if key not in stop_words and isinstance(key, str):
+            my_dict[key] = value
+    return my_dict
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    pass
-
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    """
-    pass
+def get_top_n(my_dict: dict, top_n: int):
+    list_freq=[]
+    if my_dict is {} or top_n<=0:
+        return()
+    else:
+        list_freq=sorted(my_dict, key=my_dict.get, reverse=True)
+    return tuple(list_freq[:top_n])
+    print(get_top_n(my_dict, top_n))
