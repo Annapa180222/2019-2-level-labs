924c22eb9270a493de6b8f2b8a0dee5423932d89
Annapa180222 <55386460+Annapa180222@users.noreply.github.com>
2019-12-27 09:59:55 +0300

    Update main.py
    ...

--- lab_1/main.py
+++ lab_1/main.py
@@ -1,89 +1,134 @@
-def calculate_frequences(text: str) -> dict:
-    """
-    Calculates number of times each word appears in the text
-    """
-    frequencies = {}
-    new_text = ''
-    if text is None:
-        return frequencies
-    if not isinstance(text, str):
-        text = str(text)
-    for symbol in text:
-        if symbol.isalpha() or symbol == ' ':
-            new_text += symbol
-    new_text = new_text.lower()
-    words = new_text.split()
-    for key in words:
-        key = key.lower()
-        if key in frequencies:
-            value = frequencies[key]
-            frequencies[key] = value + 1
-        else:
-            frequencies[key] = 1
-    return frequencies
+import math
+REFERENCE_TEXT = ''
+if __name__ == '__main__':
+    with open('not_so_big_reference_text.txt', 'r') as f:
+        REFERENCE_TEXT = f.read()
 
 
-def filter_stop_words(frequencies: dict, stop_words: tuple) -> dict:
-    """
-    Removes all stop words from the given frequencies dictionary
-    """
-    if frequencies is None:
-        frequencies = {}
-        return frequencies
-    for word in list(frequencies):
-        if not isinstance(word, str):
-            del frequencies[word]
-    if not isinstance(stop_words, tuple):
-        return frequencies
-    for word in stop_words:
-        if not isinstance(word, str):
-            continue
-        if frequencies.get(word) is not None:
-            del frequencies[word]
-    return frequencies
+class WordStorage:
+    def __init__(self):
+        self.storage = {}
+
+    def put(self, word: str) -> int:
+        if type(word)!= str:
+            return -1
+        if word not in self.storage:
+            if not self.storage:
+                ident = 1
+            else:
+                ident = max(self.storage.values()) + 1
+            self.storage[word] = ident
+        return self.storage[word]
+
+    def get_id_of(self, word: str) -> int:
+        if word in self.storage:
+            return self.storage.get(word)
+        return - 1
+
+    def get_original_by(self, id_of: int) -> str:
+        if type(id_of) == int and id_of in self.storage.values:
+           id_of = list(self.storage.values()).index(id_of)
+            return list(self.storage.keys())[id_of]
+        return 'UNK'
+
+    def from_corpus(self, corpus: tuple):
+        if type(corpus) == tuple:
+            return {}
+        for word in corpus:
+            if word not in self.storage:
+                self.storage[word] = self.put(word)
+        return self.storage
 
 
-def get_top_n(frequencies: dict, top_n: int) -> tuple:
-    """
-    Takes first N popular words
-    :param
-    """
-    if not isinstance(top_n, int):
-        frequencies = ()
-        return frequencies
-    if top_n < 0:
-        top_n = 0
-    elif top_n > len(frequencies):
-        top_n = len(frequencies)
-    top_words = sorted(frequencies, key=lambda x: int(frequencies[x]), reverse=True)
-    best = tuple(top_words[:top_n])
-    return best
+class NGramTrie:
+    def __init__(self, n):
+        self.gram_frequencies = {}
+        self.gram_log_probabilities = {}
+        self.size = size
+
+    def fill_from_sentence(self, sentence: tuple) -> str:
+        if sentence != tuple(sentence) or sentence == () or sentence is None:
+            return "ERROR"
+        kol_combinations = []
+        for i in range(len(sentence)):
+            if i < len(sentence) - self.size:
+                kol_combinations.append(sentence[i: i + self.size])
+            elif i == len(sentence) - self.size:
+                kol_combinations.append(sentence[i:])
+        for gram in kol_combinations:
+            if gram in self.gram_frequencies:
+                self.gram_frequencies[gram] += 1
+            else:
+                self.gram_frequencies[gram] = 1
+        return "OK"
+
+    def calculate_log_probabilities(self):
+		kol_variants=[]
+        for key in self.gram_frequencies.keys():
+            kol_variants.append(key)
+        count = 0
+        while count != len(kol_variants):
+            for gram in variants:
+                grams = []
+                gram_count = self.gram_frequencies[gram]
+                for extra_gram in kol_variants:
+                    if gram[:-1] == extra_gram[:-1]:
+                        grams.append(self.gram_frequencies[extra_gram])
+                count_grams = sum(grams)
+                e_log = math.log(gram_count / count_grams)
+                self.gram_log_probabilities[gram] = e_log
+                count = count + 1
+
+    def predict_next_sentence(self, prefix: tuple) -> list:
+        if self.gram_log_probabilities is None or type(prefix)!= tuple or or prefix == () len(prefix) != self.size - 1 :
+            return []
+        s = []
+        s.extend(list(prefix))
+        list_n = []
+        for gram in self.gram_log_probabilities:
+            gram_1 = gram[:len(gram) - 1]
+            list_n.append(gram_1)
+        while prefix in list_n:
+            prob_l = []
+            for keys, values in self.gram_log_probabilities.items():
+                if prefix == keys[:len(keys) - 1]:
+                    prob_l.append((values, keys))
+            prob_l.sort(reverse=True)
+            s.append(prob_l[0][1][-1])
+            prefix = prob_l[0][1][1:]
+        return s
 
 
-def read_from_file(path_to_file: str, lines_limit: int) -> str:
-    """
-    Read text from file
-    """
-    file = open(path_to_file)
-    counter = 0
-    text = ''
-    if file is None:
-        return text
-    for line in file:
-        text += line
-        counter += 1
-        if counter == lines_limit:
-            break
-    file.close()
-    return text
+def encode(storage_instance, corpus) -> list:
+    new_corpus = []
+    if corpus is not None:
+        for s in corpus:
+            encoded_sentence = []
+            for word in s:
+                encoded_sentence.append(storage_instance.get_id_of(word))
+            new_corpus.append(encoded_sentence)
+    else:
+        return []
+    return new_corpus
 
 
-def write_to_file(path_to_file: str, content: tuple):
-    """
-    Creates new file
-    """
-    file = open(path_to_file, 'w')
-    for i in content:
-        file.write(i)
-        file.write('\n')
-    file.close()
+def split_by_sentence(text: str) -> list:
+    sentences = []
+    if type(text)!= str or '.' not in text or text == '' :
+        return sentences
+    text = text.lower()
+    text = text.replace('\n', ' ')
+    text = text.replace('?', '.')
+    text = text.replace('!', '.')
+    text = text.split('. ')
+    for sentence in text:
+        clear = ''
+        for symbol in sentence:
+            if symbol.isalpha() or sym == ' ':
+                clear += sym
+        if clear_sentence:
+            s = ['<s>']
+            s.extend(clear.split())
+            s.append('</s>')
+            sentences.append(s)
+    return sentences
